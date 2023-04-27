148750cd1469438c758bebe1c7c7aca0527ea44d
Sasha Schwarz <Schwarz@mail.ru>
2023-04-27 22:06:04 +0300

    second commit

---
+++ class.cpp
@@ -0,0 +1,31 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.4.33213.308
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "class", "class.vcxproj", "{AAF317EC-5073-4BF0-A6A1-B1B66EB8F29A}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|x64 = Debug|x64
+		Debug|x86 = Debug|x86
+		Release|x64 = Release|x64
+		Release|x86 = Release|x86
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{AAF317EC-5073-4BF0-A6A1-B1B66EB8F29A}.Debug|x64.ActiveCfg = Debug|x64
+		{AAF317EC-5073-4BF0-A6A1-B1B66EB8F29A}.Debug|x64.Build.0 = Debug|x64
+		{AAF317EC-5073-4BF0-A6A1-B1B66EB8F29A}.Debug|x86.ActiveCfg = Debug|Win32
+		{AAF317EC-5073-4BF0-A6A1-B1B66EB8F29A}.Debug|x86.Build.0 = Debug|Win32
+		{AAF317EC-5073-4BF0-A6A1-B1B66EB8F29A}.Release|x64.ActiveCfg = Release|x64
+		{AAF317EC-5073-4BF0-A6A1-B1B66EB8F29A}.Release|x64.Build.0 = Release|x64
+		{AAF317EC-5073-4BF0-A6A1-B1B66EB8F29A}.Release|x86.ActiveCfg = Release|Win32
+		{AAF317EC-5073-4BF0-A6A1-B1B66EB8F29A}.Release|x86.Build.0 = Release|Win32
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {97F73AB3-4851-4CD7-9220-D214F279DC7A}
+	EndGlobalSection
+EndGlobal
